da1c3606f74976a9a1ac24401db0acab32c15bf5
Ruslan <ruslanlantsov1997@gmail.com>
2023-03-02 13:48:26 +0200

    searching users & finishing the app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swipe_action_cell/flutter_swipe_action_cell.dart';
+
 import 'package:phone_book/constants.dart';
 import 'package:phone_book/resources/auth_methods.dart';
 import 'package:phone_book/resources/firestore_methods.dart';
 import 'package:phone_book/screens/add_employee_screen.dart';
 import 'package:phone_book/screens/employee_detailed_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  bool _isSearching = false;
 
   void logout() async {
     await AuthMethods().signOut();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 
   @override
@@ -49,9 +65,34 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(14.0),
         child: Column(
-          children: const [
-            _SearchTextField(),
-            _EmployeesList(),
+          children: [
+            TextField(
+              onSubmitted: (String _) {
+                setState(() {
+                  _isSearching = true;
+                });
+
+                if (_searchController.text.isEmpty) {
+                  _isSearching = false;
+                }
+              },
+              autofocus: false,
+              controller: _searchController,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Search',
+                filled: true,
+                isDense: true,
+                contentPadding: const EdgeInsets.all(10),
+              ),
+            ),
+            _isSearching
+                ? _SearchList(text: _searchController.text)
+                : const _EmployeesList(),
           ],
         ),
       ),
@@ -59,29 +100,10 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class _SearchTextField extends StatelessWidget {
-  const _SearchTextField();
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        prefixIcon: const Icon(Icons.search),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide.none,
-        ),
-        hintText: 'Search',
-        filled: true,
-        isDense: true,
-        contentPadding: const EdgeInsets.all(10),
-      ),
-    );
-  }
-}
-
 class _EmployeesList extends StatelessWidget {
-  const _EmployeesList({Key? key}) : super(key: key);
+  const _EmployeesList({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -99,70 +121,113 @@ class _EmployeesList extends StatelessWidget {
             ),
           );
         }
-        return Expanded(
-          child: ListView.separated(
-            itemCount: snapshot.data!.docs.length,
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(),
-            itemBuilder: (BuildContext context, int index) {
-              final employee = snapshot.data!.docs[index];
+        return _List(snapshot: snapshot);
+      },
+    );
+  }
+}
 
-              return SwipeActionCell(
-                key: UniqueKey(),
-                trailingActions: <SwipeAction>[
-                  SwipeAction(
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                    ),
-                    onTap: (CompletionHandler handler) async {
-                      await handler(true);
-                      await FirestoreMethods().delete(
-                        employee['docId'],
-                      );
-                    },
-                    color: Colors.red,
-                  ),
-                  SwipeAction(
-                    icon: const Icon(Icons.edit, color: Colors.white),
-                    onTap: (CompletionHandler handler) async {
-                      handler(false);
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => AddEmployeeScreen(
-                            isEditMode: true,
-                            employee: employee,
-                          ),
-                        ),
-                      );
-                    },
-                    color: Colors.grey,
-                  ),
-                ],
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) => EmployeeDetailedScreen(
-                          employee: employee,
-                        ),
+class _SearchList extends StatelessWidget {
+  final String text;
+  const _SearchList({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: db
+          .collection('employees')
+          .where('companyId', isEqualTo: auth.currentUser?.uid)
+          .where('fullName', isGreaterThanOrEqualTo: text)
+          .get(),
+      builder: (BuildContext context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          );
+        }
+        return _List(snapshot: snapshot);
+      },
+    );
+  }
+}
+
+class _List extends StatelessWidget {
+  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot;
+
+  _List({
+    Key? key,
+    required this.snapshot,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: snapshot.data!.docs.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          final employee = snapshot.data!.docs[index];
+
+          return SwipeActionCell(
+            key: UniqueKey(),
+            trailingActions: <SwipeAction>[
+              SwipeAction(
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+                onTap: (CompletionHandler handler) async {
+                  await handler(true);
+                  await FirestoreMethods().delete(
+                    employee['docId'],
+                  );
+                },
+                color: Colors.red,
+              ),
+              SwipeAction(
+                icon: const Icon(Icons.edit, color: Colors.white),
+                onTap: (CompletionHandler handler) async {
+                  handler(false);
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => AddEmployeeScreen(
+                        isEditMode: true,
+                        employee: employee,
                       ),
-                    );
-                  },
-                  child: ListTile(
-                    title: Text(
-                      employee['fullName'],
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
                     ),
+                  );
+                },
+                color: Colors.grey,
+              ),
+            ],
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => EmployeeDetailedScreen(
+                      employee: employee,
+                    ),
+                  ),
+                );
+              },
+              child: ListTile(
+                title: Text(
+                  employee['fullName'],
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              );
-            },
-          ),
-        );
-      },
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
